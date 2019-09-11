996dc67190422734748db120aebe8a105a4c3136
bradleykenny <bkenny283@gmail.com>
2019-09-11 18:58:27 +1000

    575 working solution.

--- 575/Main.java
+++ 575/Main.java
@@ -4,6 +4,22 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-		
+		Scanner in = new Scanner(System.in);
+		while (in.hasNextLine()) {
+			String line = in.nextLine();
+			if (line.equals("0")) {
+				break;
+			}
+
+			System.out.println(getValue(line));
+		}
+	}
+
+	public static int getValue(String num) {
+		if (num.isEmpty()) {
+			return 0;
+		}
+
+		return Integer.parseInt(num.substring(0, 1)) * (int)(Math.pow(2, num.length()) - 1) + getValue(num.substring(1));
 	}
 }

67bcd8f6e3c0d295747498ff6fee8d3c6e175174
bradleykenny <bkenny283@gmail.com>
2019-09-12 13:43:30 +1000

    Solution to 414 -- accepted by UVa.

--- 414/Main.java
+++ 414/Main.java
@@ -6,7 +6,44 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		while (in.hasNextLine()) {
-			
+			int numLines = Integer.parseInt(in.nextLine());
+
+			if (numLines == 0) {
+				break;
+			}
+
+			String[] images = new String[numLines];
+			for (int i = 0; i < numLines; i++) {
+				images[i] = in.nextLine();
+			}
+
+			int[] numX = new int[numLines];
+			int max = 0;
+			for (int i = 0; i < numLines; i++) {
+				numX[i] = numX(images[i]);
+				if (numX[i] > max) {
+					max = numX[i];
+				}
+			}
+
+			int count = 0;
+			for (int num : numX) {
+				count += max - num;
+			}
+
+			System.out.println(count);
+		}
+	}
+
+	public static int numX(String data) {
+		if (data.length() == 0) {
+			return 0;
+		}
+		
+		if (data.charAt(0) == 'X') {
+			return 1 + numX(data.substring(1));
+		} else {
+			return 0 + numX(data.substring(1));
 		}
 	}
 }

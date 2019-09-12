18c7a6b4f15b44111b6c23729897a51f38b2fa6f
bradleykenny <bkenny283@gmail.com>
2019-09-12 13:15:27 +1000

    Setting up for p414.

---
+++ 414/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		while (in.hasNextLine()) {
+			
+		}
+	}
+}
